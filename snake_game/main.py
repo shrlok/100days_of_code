c75415680f05a4dff981795701385e9ed121e742
DESKTOP-EO34P9I\highw <highway.cry@gmail.com>
2022-09-16 00:40:00 +0300

    Fix COPY and CMD for macOS in Dockerfile Delete comment in python web server Change header in index.html

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,25 +1,39 @@
 from turtle import Screen, Turtle
+import time
 # Screen setting
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
+starting_position = ((0, 0), (-20, 0), (-40, 0))
 
-# The len of the snake's bodies
-body_len = range(3)
-# Start position
-start_x = 0
+segments = []
+
 # Snake's body description
-def snake_body(x_pos):
-    snakes_square = Turtle(shape="square")
-    snakes_square.color("white")
-    snakes_square.penup()
-    snakes_square.setposition(x= x_pos, y=0)
+def snake_body(pos):
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(pos)
+    segments.append(new_segment)
 
 # Creating snake's body
-for x in body_len:
-    snake_body(start_x)
-    start_x -=20
+for position in starting_position:
+    snake_body(position)
 
+
+game_in_on = True
+while game_in_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
+    if segments[0].xcor() >= 300:
+        game_in_on = False
 # Screen exit func
 screen.exitonclick()
